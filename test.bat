d3ad660aff8b8c8dae37cff979d635f1acc85991
VisualBrain <52631378+VisualBrain@users.noreply.github.com>
2020-01-20 17:22:08 +0530

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,33 +1,33 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM for /l %%s in (1,2,4) do echo %%s
+for /l %%s in (1,2,4) do echo %%s
 
-REM REM for /d %file in ( %cd%\Batch* ) do @echo %file
-REM REM @echo off 
-REM REM REG QUERY HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Windows\
+REM for /d %file in ( %cd%\Batch* ) do @echo %file
+REM @echo off 
+REM REG QUERY HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Windows\
 
-REM REM echo TaskList
-REM REM TaskList
-REM echo -------------------------------------------------------------------
-REM echo OS INFO
-REM echo -------------------------------------------------------------------
-REM systeminfo | findstr /c:"OS Name"
-REM systeminfo | findstr /c:"OS Version"
-REM systeminfo | findstr /c:"System Type"
-REM echo -------------------------------------------------------------------
-REM echo HARDWARE INFO
-REM echo -------------------------------------------------------------------
-REM systeminfo | findstr /c:"Total Physical Memory"
-REM wmic cpu get name
-REM echo -------------------------------------------------------------------
-REM echo NETWORK INFO
-REM echo -------------------------------------------------------------------
-REM ipconfig | findstr IPv4
-REM ipconfig | findstr IPv6
-REM git init
-REM git add --all
-REM git commit -m "JALAL"
+REM echo TaskList
+REM TaskList
+echo -------------------------------------------------------------------
+echo OS INFO
+echo -------------------------------------------------------------------
+systeminfo | findstr /c:"OS Name"
+systeminfo | findstr /c:"OS Version"
+systeminfo | findstr /c:"System Type"
+echo -------------------------------------------------------------------
+echo HARDWARE INFO
+echo -------------------------------------------------------------------
+systeminfo | findstr /c:"Total Physical Memory"
+wmic cpu get name
+echo -------------------------------------------------------------------
+echo NETWORK INFO
+echo -------------------------------------------------------------------
+ipconfig | findstr IPv4
+ipconfig | findstr IPv6
+git init
+git add --all
+git commit -m "JALAL"
 :argactionstart
 if -%1-==-- goto argactionend
 echo %1 & rem  do anything
